8d623cf1e38967020a0519f773f406394ab5811d
Jacob <1176699766@qq.com>
2021-04-21 07:53:56 +0800

    增加了一些组件

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,15 +3,20 @@
       <nav-bar>
         <span slot="center">购物街</span>
       </nav-bar>
+
+      <swiper :banners="[]"></swiper>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/NavBar/NavBar'
+import Swiper from './ChildComps/HomeSwiper'
+
 export default {
     name:'home',
     components:{
-      NavBar
+      NavBar,
+      Swiper
     }
 }
 </script>
